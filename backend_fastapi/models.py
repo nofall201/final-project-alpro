f37884b49378c3bb50efd510174d7368e54cce6e
auliamnaufal <auliamnaufal@gmail.com>
2025-12-16 19:25:28 +0700

    Feat: change logic

--- backend_fastapi/models.py
+++
@@ -1,9 +0,0 @@
-from sqlmodel import SQLModel, Field
-from datetime import datetime
-
-class Detection(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    timestamp: datetime
-    person_count: int
-    violation_count: int
-    screenshot_path: str | None
