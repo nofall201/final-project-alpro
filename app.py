c78cb5b14f997170938512e5181f7ab327e9c41d
Naufal Afif <nofall201@gmail.com>
2025-12-16 22:18:36 +0700

    fix railway crash: allow unsafe werkzeug

--- app.py
+++ app.py
@@ -87,4 +87,10 @@ app = create_app()
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    socketio.run(app, host="0.0.0.0", port=port, debug=True)
+    socketio.run(
+    app,
+    host="0.0.0.0",
+    port=port,
+    debug=False,
+    allow_unsafe_werkzeug=True
+)
